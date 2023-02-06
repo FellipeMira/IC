6c88f5469843c9c3caa9f37d392cb84fb84f919c
FellipeMira <fellipe.mira@unesp.br>
2023-02-06 10:07:52 -0300

    ajustes mesclados

--- std_bfast.R
+++ std_bfast.R
@@ -4,22 +4,42 @@ library(lubridate)
 library(purrr)
 library(furrr)
 
-path <- "~/Desktop/Python/IC/scripts/IC/.ipynb_checkpoints/Cods.Mira/"
+
+
+path <- "C:/Users/Fellipe/Desktop/Projetos/IC/IC/data/"
 setwd(path)
 
-files_median <- list.files(path = path,pattern = '*std*')
-files_std <- list.files(path = path,pattern = '*median*')
 
-df_median <- lapply(files_median,function(x){read_csv(x) %>% filter(lubridate::year(Timestamp) %in% c(2008:2021))})
-df_std <- lapply(files_std,function(x){return(read_csv(x) %>%
-                                                select(c(NDVI,Timestamp)) %>%
-                                                filter(lubridate::year(Timestamp) %in% c(2008:2021)))})
+########################### FUNCTION ###########################################
 
-ts_median <- lapply(df_median, function(x){bfastts(data = x$NDVI,dates = x$Timestamp, type = c("irregular"))})
+time.series.to.dataframe <- function(time_series, source) {
+  s.df           <- data.frame(as.numeric(time_series))
+  colnames(s.df) <- "NDVI"
+  s.df$Time      <- as.Date(date_decimal(as.numeric(time(time_series))))
+  s.df$Type      <- source
+  return(s.df)
+}
+
+################################################################################
+
+files_median <- list.files(path = path,pattern = '^df_median*')
+
+files_std <- list.files(path = path,pattern = '^df_std*')
+
+df_median <- lapply(files_median,function(x){return(read_csv(x))})
+
+df_std <- lapply(files_std,function(x){return(read_csv(x))})
+
+ts_median <- lapply(df_median, function(x){bfastts(data = x$NDVI,
+                                                   dates = x$Timestamp,
+                                                   type = c("irregular"))})
+
+df_median[[1]]$Timestamp %>% summary 
+
 s.d.periodic <- lapply(ts_median, function(x){round(forecast::na.interp(x),4)})
 
 fit <- list()
-for (i in seq(1,4)) {
+for (i in seq(1,9)) {
   fit[[i]] <- bfast(Yt = s.d.periodic[[i]], h = 50/length(s.d.periodic[[i]]), season = "harmonic", breaks = 4, max.iter = 2) 
   print(paste0('iteração:',i))
   gc()
@@ -32,12 +52,12 @@ St<- map(fit,function(x){x[["output"]][[1]][["St"]]})
 St[[1]] %>% plot()
 
 St_df <- list() 
-for (x in 1:4) {
+for (x in 1:9) {
   St_df[[x]] <- time.series.to.dataframe(St[[x]],'none')
   }  
 
 
-for(i in 1:4){
+for(i in 1:9){
   write.csv(St_df[[i]], file = paste0('St_df',i,".csv"))
 }
 
@@ -48,8 +68,11 @@ df_merged <- map2(St_df,
                   df_std,
                   function(x,y){return(left_join(x,y,
                                                  by=c('Time'='Timestamp'),
-                                                 suffix = c(".bfast", ".std")))},
-                  .progress = T)
+                                                 suffix = c(".bfast", ".std")))})
+
+for(i in 1:9){
+  write.csv(df_merged[[i]], file = paste0('merged_df',i,".csv"))
+}
 
 St_df[[9]] %>% glimpse
 df_std[[9]] %>%  glimpse
@@ -107,12 +130,6 @@ df_merged[[1]] %>%
   theme_linedraw()
 
 ###############################################################################
-time.series.to.dataframe <- function(time_series, source) {
-    s.df           <- data.frame(as.numeric(time_series))
-    colnames(s.df) <- "NDVI"
-    s.df$Time      <- as.Date(date_decimal(as.numeric(time(time_series))))
-    s.df$Type      <- source
-    return(s.df)
-}
+
 
 getwd()
